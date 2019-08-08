602a712aa95ac81d3b99347d2f5b74a4c8e5668b
sonaksh <sonaksh@swiftace.ai>
2019-08-08 20:34:19 +0530

    added routes(react-router-dom)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+const Home = () => (
+  <div>
+    Home
+  </div>
+)
+
+const About = () => (
+  <div>
+    About
+  </div>
+)
+
+const Code = () => (
+  <div>
+    Code
+  </div>
+)
+
+const Contact = () => (
+  <div>
+    Contact
+  </div>
+)
+
+const Info = () => (
+  <div>
+    info
+  </div>
+)
+
+const MainMenu = () => {
+return (
+<div>
+  <Link to="/">
+    <button>home</button>
+  </Link>
+  <Link to="/about">
+    <button>About</button>
+  </Link>
+  <Link to="/code">
+    <button>code</button>
+  </Link>
+  <Link to="/contact">
+    <button>contact</button>
+  </Link>
+  <Link to="/info">
+    <button>info</button>
+  </Link>
+</div>
+);
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <MainMenu />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/code" component={Code} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/info" component={Info} />
+          </div>
+        </header>
+        
+      </div>
+    </Router>
   );
 }
 
